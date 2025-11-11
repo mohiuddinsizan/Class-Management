b4a065ae1bd44fcf2dfe0f5f75d84136a32f9bab
mohiuddinsizan <mohiuddinsizan13@gmail.com>
2025-11-11 20:33:35 +0600

    nothing

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -15,7 +15,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE ||
-    "https://class-management-rwvm.onrender.com/api",
+    "https://class-management-1-80lu.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
